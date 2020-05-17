818b4ab911487bbef09b2c65e5f7d340e680b172
lixianjing <xianjimli@hotmail.com>
2020-05-17 18:31:59 +0800

    format code

--- src/spiffs/spiffs_ram.c
+++ src/spiffs/spiffs_ram.c
@@ -1,64 +1,72 @@
-﻿#include "spiffs.h"
+#include "spiffs.h"
+#include <assert.h>
 
-#define RAM_SIZE 100 * 1024
-
-static u8_t s_flash[RAM_SIZE];
+static u8_t* s_flash;
+static uint32_t s_flash_size;
 
 static s32_t _read(
 #if SPIFFS_HAL_CALLBACK_EXTRA
-    spiffs *fs,
+    spiffs* fs,
 #endif
-    u32_t addr, u32_t size, u8_t *dst) {
+    u32_t addr, u32_t size, u8_t* dst) {
+  assert((addr + size) <= s_flash_size);
   memcpy(dst, s_flash + addr, size);
   return 0;
 }
 
 static s32_t _write(
 #if SPIFFS_HAL_CALLBACK_EXTRA
-    spiffs *fs,
+    spiffs* fs,
 #endif
-    u32_t addr, u32_t size, u8_t *src) {
+    u32_t addr, u32_t size, u8_t* src) {
   int i;
+  assert((addr + size) <= s_flash_size);
+  memcpy(s_flash + addr, src, size);
 
-  for (i = 0; i < size; i++) {
-    s_flash[addr+i] = src[i]; 
-  }
   return 0;
 }
 static s32_t _erase(
 #if SPIFFS_HAL_CALLBACK_EXTRA
-    spiffs *fs,
+    spiffs* fs,
 #endif
     u32_t addr, u32_t size) {
-  memset(s_flash+addr, 0xff, size);
+  assert((addr + size) <= s_flash_size);
+  memset(s_flash + addr, 0xff, size);
   return 0;
 }
 
 static void spiffs_check_cb_f(
 #if SPIFFS_HAL_CALLBACK_EXTRA
-        spiffs *fs,
+    spiffs* fs,
 #endif
-    spiffs_check_type type, spiffs_check_report report,
-    u32_t arg1, u32_t arg2) {
+    spiffs_check_type type, spiffs_check_report report, u32_t arg1, u32_t arg2) {
+  return;
 }
 
+static u8_t _fds[64];
 static u8_t _work[512];
-static u8_t _fds[256];
-static u32_t _fds_sz = 256;
 static u8_t _cache[4096];
+static u32_t _fds_sz = 64;
 static u32_t _cache_sz = 4096;
 
-s32_t fs_mount_ram(spiffs* fs) {
+s32_t fs_mount_ram(spiffs* fs, void* start_addr, uint32_t size) {
   spiffs_config c;
+
+  memset(&c, 0x00, sizeof(c));
+  s_flash = start_addr;
+  s_flash_size = size;
+
   c.hal_erase_f = _erase;
   c.hal_read_f = _read;
   c.hal_write_f = _write;
+
 #if SPIFFS_SINGLETON == 0
-  c.log_block_size = 4096;
+  c.phys_erase_block = 512;
+  c.log_block_size = 1024;
   c.log_page_size = 256;
+  c.phys_size = size;
   c.phys_addr = 0;
-  c.phys_erase_block = 4096;
-  c.phys_size = RAM_SIZE;
 #endif
+
   return SPIFFS_mount(fs, &c, _work, _fds, _fds_sz, _cache, _cache_sz, spiffs_check_cb_f);
 }
